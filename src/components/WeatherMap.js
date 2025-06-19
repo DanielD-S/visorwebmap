a9893e2f14cbd233ab1d30fda9e94cf6d685b81b
DanielD-S <daniel.diazsantander@gmail.com>
2025-06-19 16:21:21 -0400

    Fix conflicto entre mapa y gráficos Chart.js

--- src/components/WeatherMap.js
+++ src/components/WeatherMap.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-// Corrige íconos de marcador en Leaflet para Vite/React
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
 });
+
+function MapUpdater({ lat, lon }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (lat && lon) {
+      map.setView([lat, lon], 14);
+    }
+  }, [lat, lon, map]);
+
+  return null;
+}
 
 function WeatherMap({ lat, lon, id }) {
   if (!lat || !lon) return null;
@@ -24,6 +35,7 @@ function WeatherMap({ lat, lon, id }) {
         <Marker position={[lat, lon]}>
           <Popup>Torre: {id}</Popup>
         </Marker>
+        <MapUpdater lat={lat} lon={lon} />
       </MapContainer>
     </div>
   );
